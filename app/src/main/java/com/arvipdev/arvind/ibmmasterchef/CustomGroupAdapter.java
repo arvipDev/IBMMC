1e13222d64aad04d302fac4f5b2116432ab680f6
arvipDev <arvind.purushotham@gmail.com>
2018-08-25 07:47:32 +0530

    initialization of list view creation

--- app/src/main/java/com/arvipdev/arvind/ibmmasterchef/CustomGroupAdapter.java
+++ app/src/main/java/com/arvipdev/arvind/ibmmasterchef/CustomGroupAdapter.java
@@ -1,5 +1,20 @@
 package com.arvipdev.arvind.ibmmasterchef;
 
 
-public class CustomGroupAdapter {
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+public class CustomGroupAdapter extends ArrayAdapter<String> implements View.OnClickListener{
+
+    public CustomGroupAdapter(Context context, int resource) {
+        super(context, resource);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
 }
